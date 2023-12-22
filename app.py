4f8362396bc9afb77aa8c0ded6ed414955b9ce6f
Ayancodes2003 <ayanbiswa.dutta2022@vitstudent.ac.in>
2023-12-22 14:13:09 +0530

    second

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
 @app.route("/about")
-def oli():
-    name = "oli"
-    return render_template ('about.html', name = name)
+def harry():
+    name = "harry"
+    return render_template("about.html", name2= name)
 app.run(debug=True)
